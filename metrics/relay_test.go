85e60cfe8285fde1219f734684f14495a7c589a3
Douglas Hubler <douglas@hubler.us>
2017-10-01 19:29:24 -0400

    remove unnec. deps on examples

--- metrics/relay_test.go
+++ metrics/relay_test.go
@@ -1,16 +1,40 @@
 package metrics
 
-import "testing"
-import "github.com/c2stack/c2g/device"
-import "github.com/c2stack/examples/car"
-import "github.com/c2stack/c2g/meta"
-import "github.com/c2stack/c2g/c2"
+import (
+	"testing"
 
-func Test_RelayNotify(t *testing.T) {
-	d := device.New(&meta.FileStreamSource{Root: "../vendor/github.com/c2stack/examples/car"})
-	c := car.New()
-	c.Speed = 1
-	d.Add("car", car.Manage(c))
+	"github.com/c2stack/c2g/c2"
+	"github.com/c2stack/c2g/device"
+	"github.com/c2stack/c2g/meta"
+	"github.com/c2stack/c2g/node"
+	"github.com/c2stack/c2g/nodes"
+)
+
+func TestRelayNotify(t *testing.T) {
+	d := device.New(&meta.StringSource{
+		Streamer: func(string) (string, error) {
+			return `module x {
+				revision 0;
+				notification update {		
+				}
+			}`, nil
+		},
+	})
+	update := make(chan bool)
+	n := &nodes.Basic{
+		OnNotify: func(r node.NotifyRequest) (node.NotifyCloser, error) {
+			go func() {
+				<-update
+				r.Send(nil)
+			}()
+			return func() error {
+				return nil
+			}, nil
+		},
+	}
+	if err := d.Add("x", n); err != nil {
+		t.Error(err)
+	}
 	dm := device.NewMap()
 	dm.Add("dev0", d)
 
@@ -18,7 +42,7 @@ func Test_RelayNotify(t *testing.T) {
 	r := &Relay{devices: dm, Sink: queue}
 	o := r.Source()
 	o.Path = "update"
-	o.Module = "car"
+	o.Module = "x"
 	o.Device = "dev0"
 	r.Script = `
 	  tag("i", "1")
@@ -28,7 +52,7 @@ func Test_RelayNotify(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	c.Start()
+	update <- true
 	m := <-queue
 	c2.AssertEqual(t, "1", m.Tags["i"])
 	t.Log("got one")
